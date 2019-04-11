062cce7d3cca74a401eb3f139397ab2866fa7551
Hajjar <devops.as.girl@gmail.com>
2019-04-11 12:40:01 +0200

    2 commit

---
+++ src/test/java/android/Pages/AbstractTest.java
@@ -0,0 +1,20 @@
+package android.Pages;
+
+import android.Framework.AndroidConfigClass;
+import android.Framework.ExtentReportManager;
+import android.Operations.*;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.junit.BeforeClass;
+
+public abstract  class AbstractTest {
+
+    public static ExtentReports a;
+    public static ExtentTest test;
+    ExtentReportManager r= new ExtentReportManager ();
+
+
+
+
+
+}
---
+++ src/test/java/android/Pages/SignInTest.java
@@ -0,0 +1,68 @@
+package android.Pages;
+
+import android.Framework.ExtentReportManager;
+import com.aventstack.extentreports.Status;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+/**
+ * Created by FatimaZahra on 31/01/2017.
+ */
+
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class SignInTest extends AbstractTest{
+
+    @BeforeClass
+    public static void beforeClass() {
+
+        a = ExtentReportManager.GetExtent();
+    }
+    int var=1;
+    @Test
+    public void test_01() throws Exception {
+        test = a.createTest("SignIn: test_01", "");
+
+        Assert.assertTrue(var == 1, "test passe ");
+    }
+    @Test
+    public void test_02() throws Exception {
+        test = a.createTest("SignIn: test_01", "");
+
+        Assert.assertTrue(var == 2, "test fail ");
+    }
+
+
+    @AfterMethod
+    public void getResult(ITestResult result){
+
+        if(result.getStatus() == ITestResult.FAILURE){
+
+            test.log(Status.FAIL, "Test Case Failed is "+result.getName());
+            test.log(Status.FAIL, "Test Case Failed is "+result.getThrowable());
+
+
+
+
+        }else if(result.getStatus() == ITestResult.SUCCESS){
+            test.log(Status.PASS, "Test Case success is "+result.getName());
+
+        }else if(result.getStatus() == ITestResult.SKIP){
+            test.log(Status.SKIP, "Test Case Skipped is "+result.getName());
+        }
+    }
+    @AfterClass
+    public static void tearDown(){
+
+
+        a.flush();
+
+    }
+
+
+}
